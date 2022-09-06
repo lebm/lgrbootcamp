4277ca902c7efbf0ce546914b347513378511cbc
Luis Bravo <ledbrv@gmail.com>
2022-09-06 11:30:33 -0300

    Watching datatypes video

---
+++ datatypes/src/main.rs
@@ -0,0 +1,22 @@
+#[allow(unused_variables)]
+fn main() {
+    let b1: bool = true;
+
+    let u1: u8 = 1;
+    let u2: u16 = 1;
+    let u3: u32 = 1;
+    let u4: u64 = 1;
+    let u5: u128 = 1;
+
+    let s1: i8 = 1;
+    let s2: i16 = 1;
+    let s3: i32 = 1;
+    let s4: i64 = 1;
+    let s5: i128 = 1;
+
+    let f1: f32 = 1.0;
+    let f2: f64 = 1.0;
+
+    let p1: usize = 1;
+    let p2: isize = 1;
+}
