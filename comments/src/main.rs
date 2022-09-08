fdceff52a9c031d3e7ee209bbb7eab6bacc5c00a
Luis Bravo <ledbrv@gmail.com>
2022-09-08 10:17:08 -0300

    Watched comment video.

---
+++ comments/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // This is a line comment.
+    println!("Hello, line comment!");
+
+    /*
+     *  And this a block comment.
+     *  The "*" starting this line and the previous one has no meaning, it is just formating.
+     *  Block comment delimters are '/*' and '*/'.
+     */
+    println!("Hello, block comment!");
+}
