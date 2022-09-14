b502b919db95f5ec62a1a561d61748f35b308e30
Luis Bravo <ledbrv@gmail.com>
2022-09-14 10:10:36 -0300

    Watched Tuple Structs video

--- tuple_structs/src/main.rs
+++ tuple_structs/src/main.rs
@@ -1,3 +1,24 @@
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    // tuples
+    let rgb_color = (255, 106, 0);
+    let cmyk_color = (0, 58, 100, 0);
+
+    // tuples structs
+    // More type information/meaning. Allows compiler to check types.
+    #[derive(Debug)]
+    struct RGB(i32, i32, i32);
+    #[derive(Debug)]
+    struct CMYK(i32, i32, i32, i32);
+
+    let color1 = RGB(255, 50, 0);
+    let color2 = CMYK(0, 58, 100, 0);
+
+    println!("{:?}", color1);
+    println!("{:?}", color2);
+
+    // unit struct.
+    // Used to implement traits under certain conditions.
+    #[allow(dead_code)]
+    struct MyStruct;
 }
