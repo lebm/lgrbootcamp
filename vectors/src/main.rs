fa01b3d56b68199f39ec0522cd65e7dc5ed38259
Luis Bravo <ledbrv@gmail.com>
2022-09-15 09:31:34 -0300

    Watched vectors video

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(unused_variables)]
 fn main() {
     #[allow(unused_variables)]
     let v1: Vec<String> = Vec::new();
@@ -7,7 +8,35 @@ fn main() {
     v2.push(String::from("Two"));
     v2.push(String::from("Three"));
 
-    let v2 = vec![1, 2, 3];
+    let v3 = vec![1, 2, 3];
 
     let s = &v2[0];
+    // let s v2.remove(0);
+
+    let s = v2.get(0);
+
+    if let Some(e) = s {
+        println!("{e}");
+    }
+
+    // mutable borrow of v2 allows to mutate it without copying taking ownership
+    // s will mutable borrow each of the elements v2 elements in sequence
+    for s in &mut v2 {
+        s.push_str("!");
+    }
+
+    for s in &v2 {
+        println!("{s}");
+    }
+
+    // Don't need to annotate the type. The compiler will infer from the loop below.
+    let mut v3 = vec![];
+
+    // v2 will be consumed by the loop
+    for s in v2 {
+        v3.push(s);
+    }
+
+    // Erro. v2 is now unitialized
+    //let i = v2.get(0);
 }
