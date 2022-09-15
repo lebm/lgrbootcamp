d27f317a3fa10aba38e15f916a0241726c405bae
Luis Bravo <ledbrv@gmail.com>
2022-09-15 19:16:06 -0300

    Watched the External Dependencies video

--- auth_service_file_modules2/src/lib.rs
+++ auth_service_file_modules2/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code, unused_variables)]
+use rand::prelude::*;
 
 // The database definition are in database.rs file
 mod auth_utils;
@@ -11,6 +12,8 @@ pub use auth_utils::models::Credentials;
 use database::Status;
 
 pub fn authenticate(creds: Credentials) {
+    let timeout = thread_rng().gen_range(100..500);
+    println!("The timeout is {timeout}");
     if let Status::Connected = database::connect_to_database() {
         auth_utils::login(creds);
     }
