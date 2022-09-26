f5c556dbfdaef365ca9878d9a6b1444fadaf313a
Luis Bravo <ledbrv@gmail.com>
2022-09-26 09:26:10 -0300

    Tests with doc comments.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,8 @@
+//! Let´s start with the infamous "Hello World!  
+//! * "fn" starts a function declaration.  
+//! * "main()" is the entry point of the program.  
+//! * "println!"" is a macro (ends with "!) that display the values passed as arguments.  
+//! * "Hello World!" is the a "str" (string slice), but println! receives a "&str" (reference to a string slice). In practice, "str" and "&str" are called "string slice".  
 fn main() {
     println!("Hello, world!");
 }
