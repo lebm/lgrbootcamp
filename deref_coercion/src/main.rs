b88a38588ee69cc7e680c37d90e9639aadb8684d
Luis Bravo <ledbrv@gmail.com>
2022-10-04 10:34:19 -0300

    Watched Deref Coercion video

---
+++ deref_coercion/src/main.rs
@@ -0,0 +1,43 @@
+use std::ops::{Deref, DerefMut};
+
+struct MySmartPointer<T> {
+    value: T,
+}
+
+impl<T> MySmartPointer<T> {
+    fn new(value: T) -> Self {
+        MySmartPointer { value }
+    }
+}
+
+impl<T> Deref for MySmartPointer<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T> DerefMut for MySmartPointer<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
+
+fn main() {
+    let s = MySmartPointer::new(Box::new("Hello world!".to_owned()));
+
+    // Deref coercion "converts" one reference to another.
+    // Works on types that implement Deref and DerefMut traits.
+    // &MySmartPointer -> &Box -> &String -> &str
+    print(&s);
+
+    let s0 = &s;
+    let s1 = &(*s);
+    let s2 = &(**s);
+    let s3 = &(***s);
+}
+
+fn print(s: &str) {
+    println!("{s}");
+}
